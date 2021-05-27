96b01f0cac7478cc856de2c881f9a6ba1be6d763
Suresh Ramappa <suriram.sam@gmail.com>
2021-05-27 23:53:57 +0530

    Initial files

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const PrimaryBlue = "#030086";
+
+const Button = styled.button`
+  padding: 12px 24px;
+  font-size: 1rem;
+  border-radius: 2px;
+  min-width: 100px;
+  cursor: pointer;
+  font-family: "Roboto Mono" monospace;
+`;
+
+const PrimaryButton = styled.button`
+  background-color: ${PrimaryBlue};
+  border: none;
+  color: white;
+  padding: 12px 24px;
+  font-size: 1rem;
+`;
+
+export default PrimaryButton;
