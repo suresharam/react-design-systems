2e1c066eb31057f35ac59423791e471bd4e38ce9
Suresh Ramappa <suriram.sam@gmail.com>
2021-05-28 01:55:06 +0530

    Global them

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,66 @@
 import styled from 'styled-components';
-
-const primaryBlue = "#030086"; 
+import {defaultTheme, typeScale} from '../utils';
 
 const Button = styled.button`
   padding: 8px 12px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
-  font-family: "Roboto Mono", monospace;
-`;
+  font-family: ${defaultTheme.primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear;
+
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  /* &:disabled {
+    cursor: not-allowed;
+    background: none;
+    color: ${defaultTheme.disabled};
+    border: none;
+  } */
+  `;
 
 export const PrimaryButton = styled(Button)`
-  background-color: ${primaryBlue};
-  color: white;
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
   border: 2px solid transparent;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    border-color: ${defaultTheme.disabled};
+  }
 `;
 
 export const SecondaryButton = styled(Button)`
   background: none;
-  border: 2px solid ${primaryBlue};
-  color: ${primaryBlue};
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background-color: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
   background: none;
   border: none;
-  color: ${primaryBlue};
+  color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background-color: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import  { PrimaryButton, SecondaryButton, TertiaryButton } from './components/Button';
+import { GlobalStyles } from './utils';
 
 const App = () => (
   <div>
     <PrimaryButton>Hello world</PrimaryButton>
     <SecondaryButton>Goodbye world</SecondaryButton>
     <TertiaryButton>Hey world</TertiaryButton>
+    <GlobalStyles />
   </div>
 )
 ReactDOM.render(<App />, document.getElementById('root'));
